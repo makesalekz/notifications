31985fdf16652bf8eb2e3a5dcdee16a63b405ae5
Sergei Sobolev <s.sobolev@lvi.kz>
2023-11-14 12:09:00 +0600

    ci fix

--- cmd/notifications/main.go
+++ cmd/notifications/main.go
@@ -23,7 +23,7 @@ var (
 	// Name is the name of the compiled software.
 	Name string = "notifications"
 	// Version is the version of the compiled software.
-	Version string = "1.0.0"
+	Version string = "0.1.0"
 	// flagconf is the config flag.
 	flagconf string
 

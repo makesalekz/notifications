1c447de5ce9432a45d349fe5209c369c7fcb28e7
Sergei Sobolev <s.sobolev@calendaria.ai>
2024-02-09 09:35:11 +0600

    fcm messages json tags

--- messages/fcm.go
+++ messages/fcm.go
@@ -3,10 +3,10 @@ package messages
 import "gitlab.calendaria.team/services/notifications/ent/enum"
 
 type FirebaseNotification struct {
-	Type     enum.NotificationType
-	UsersIds []int64
-	Title    string
-	Body     string
-	Image    string
-	Data     map[string]string
+	Type     enum.NotificationType `json:"type,omitempty"`
+	UsersIds []int64               `json:"users_ids"`
+	Title    string                `json:"title,omitempty"`
+	Body     string                `json:"body,omitempty"`
+	Image    string                `json:"image,omitempty"`
+	Data     map[string]string     `json:"data,omitempty"`
 }

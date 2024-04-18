fe07bdae8b2a97976ca9ad7bdf69f3358a0c1432
Aidar Kudabaev <a.kudabaev@calendaria.ai>
2024-04-18 16:02:37 +0500

    Dockerfile

--- internal/data/localizer.go
+++ internal/data/localizer.go
@@ -15,13 +15,13 @@ type Localizer struct {
 func NewLocalizer() (*Localizer, error) {
 	bundle := i18n.NewBundle(language.English)
 	bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
-	files, err := os.ReadDir("locales/")
+	files, err := os.ReadDir("./locales/")
 	if err != nil {
 		return nil, err
 	}
 
 	for _, file := range files {
-		bundle.MustLoadMessageFile("locales/" + file.Name())
+		bundle.MustLoadMessageFile("./locales/" + file.Name())
 	}
 
 	return &Localizer{

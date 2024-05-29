726bed23176f1fde283c3c9529fe7e25bef39f35
s.mukhambetov <s.mukhambetov@calendaria.ai>
2024-05-29 18:01:19 +0500

    Fix: Removed validator form http server

--- internal/server/http.go
+++ internal/server/http.go
@@ -4,7 +4,6 @@ import (
 	prom "github.com/go-kratos/kratos/contrib/metrics/prometheus/v2"
 	"github.com/go-kratos/kratos/v2/middleware/metadata"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
-	"github.com/go-kratos/kratos/v2/middleware/validate"
 	"github.com/go-kratos/kratos/v2/transport/http"
 	khttp "github.com/go-kratos/kratos/v2/transport/http"
 	"github.com/prometheus/client_golang/prometheus"
@@ -47,7 +46,6 @@ func NewHTTPServer(
 			recovery.Recovery(),
 			metadata.Server(),
 			auth.Server(jwtp),
-			validate.Validator(),
 			metrics.Server(
 				metrics.WithSeconds(prom.NewHistogram(_metricSeconds)),
 				metrics.WithRequests(prom.NewCounter(_metricRequests)),

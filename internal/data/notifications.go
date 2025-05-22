2b3ec7dadc98ed551b0fb40aa0031532e662f256
s.mukhambetov <s.mukhambetov@calendaria.ai>
2025-05-22 23:57:38 +0500

    Fixed some issues

--- internal/data/notifications.go
+++ internal/data/notifications.go
@@ -1,4 +1,3 @@
-// nolint: gosec // convertation to int32 is safe
 package data
 
 import (
@@ -110,6 +109,7 @@ func (r *notificationsRepo) CreateNotifications(ctx context.Context, data []*Not
 		return 0, err
 	}
 
+	// nolint:gosec // convertation to int32 is safe
 	return int32(created), err
 }
 

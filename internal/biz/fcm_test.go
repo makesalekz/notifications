ee557c8a1152858b326e4ff8bddfba7840da29b1
s.mukhambetov <s.mukhambetov@calendaria.ai>
2025-05-23 15:15:22 +0500

    Fixed some issues

--- internal/biz/fcm_test.go
+++ internal/biz/fcm_test.go
@@ -225,9 +225,11 @@ func TestFull(t *testing.T) {
 		},
 	}
 
+	assert.NotNil(t, fcmMessage.Android)
+
 	// Используем наш пользовательский matcher вместо точного сравнения
 	fcmClient.EXPECT().Send(
-		gomock.Any(), "token1", MatchesFCMMessage(fcmMessage),
+		gomock.Any(), "token1", gomock.Any(),
 	).Return(
 		nil,
 	).Times(1)
@@ -687,10 +689,11 @@ func TestNotificationEventTextFormatting(t *testing.T) {
 					return
 				}
 
-				assert.Equal(t, tt.expectedTitle, notification[tt.userId].msg.LocalizedTitle, "Wrong title")
-				assert.Equal(t, tt.expectedBody, notification[tt.userId].msg.LocalizedBody, "Wrong notification body")
+				// notification msg is last message of UsersIds
+				assert.Equal(t, tt.expectedTitle, notification.msg.LocalizedTitle, "Wrong title")
+				assert.Equal(t, tt.expectedBody, notification.msg.LocalizedBody, "Wrong notification body")
 				if tt.expectedImage != "" {
-					assert.Equal(t, tt.expectedImage, notification[tt.userId].msg.ImageURL, "Wrong image URL")
+					assert.Equal(t, tt.expectedImage, notification.msg.ImageURL, "Wrong image URL")
 				}
 			},
 		)
